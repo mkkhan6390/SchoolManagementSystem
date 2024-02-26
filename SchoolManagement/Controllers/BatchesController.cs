bfc1505fae2e11a9914b7f8e40fde6f687b87620
mkkhan6390 <mkkhan6390@gmail.com>
2024-02-26 09:46:14 -0800

    Adding Project To github

---
+++ SchoolManagement/Controllers/BatchesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SchoolManagement.Data;
+
+namespace SchoolManagement.Controllers
+{
+    public class BatchesController : Controller
+    {
+        private SchoolManagementEntities db = new SchoolManagementEntities();
+
+        // GET: Batches
+        public ActionResult Index()
+        {
+            var batches = db.Batches.Include(b => b.Teacher);
+            return View(batches.ToList());
+        }
+
+        // GET: Batches/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Batch batch = db.Batches.Find(id);
+            if (batch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(batch);
+        }
+
+        // GET: Batches/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClassTeacherId = new SelectList(db.Teachers, "Id", "Name");
+            return View();
+        }
+
+        // POST: Batches/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ClassTeacherId,Class,Div,isDeleted,CreatedBy,CreatedDate,ModifiedBy,ModifiedDate")] Batch batch)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Batches.Add(batch);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClassTeacherId = new SelectList(db.Teachers, "Id", "Name", batch.ClassTeacherId);
+            return View(batch);
+        }
+
+        // GET: Batches/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Batch batch = db.Batches.Find(id);
+            if (batch == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClassTeacherId = new SelectList(db.Teachers, "Id", "Name", batch.ClassTeacherId);
+            return View(batch);
+        }
+
+        // POST: Batches/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ClassTeacherId,Class,Div,isDeleted,CreatedBy,CreatedDate,ModifiedBy,ModifiedDate")] Batch batch)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(batch).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClassTeacherId = new SelectList(db.Teachers, "Id", "Name", batch.ClassTeacherId);
+            return View(batch);
+        }
+
+        // GET: Batches/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Batch batch = db.Batches.Find(id);
+            if (batch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(batch);
+        }
+
+        // POST: Batches/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Batch batch = db.Batches.Find(id);
+            db.Batches.Remove(batch);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SchoolManagement/Controllers/LecturesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SchoolManagement.Data;
+
+namespace SchoolManagement.Controllers
+{
+    public class LecturesController : Controller
+    {
+        private SchoolManagementEntities db = new SchoolManagementEntities();
+
+        // GET: Lectures
+        public ActionResult Index()
+        {
+            var lectures = db.Lectures.Include(l => l.Batch).Include(l => l.Subject).Include(l => l.Teacher);
+            return View(lectures.ToList());
+        }
+
+        // GET: Lectures/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lecture lecture = db.Lectures.Find(id);
+            if (lecture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lecture);
+        }
+
+        // GET: Lectures/Create
+        public ActionResult Create()
+        {
+            ViewBag.BatchId = new SelectList(db.Batches, "Id", "Div");
+            ViewBag.SubjectId = new SelectList(db.Subjects, "Id", "Name");
+            ViewBag.TeacherId = new SelectList(db.Teachers, "Id", "Name");
+            return View();
+        }
+
+        // POST: Lectures/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,TeacherId,SubjectId,ScheduleDate,StartTime,EndTime,Hours,isDeleted,CreatedBy,CreatedDate,ModifiedBy,ModifiedDate,BatchId")] Lecture lecture)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Lectures.Add(lecture);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BatchId = new SelectList(db.Batches, "Id", "Div", lecture.BatchId);
+            ViewBag.SubjectId = new SelectList(db.Subjects, "Id", "Name", lecture.SubjectId);
+            ViewBag.TeacherId = new SelectList(db.Teachers, "Id", "Name", lecture.TeacherId);
+            return View(lecture);
+        }
+
+        // GET: Lectures/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lecture lecture = db.Lectures.Find(id);
+            if (lecture == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BatchId = new SelectList(db.Batches, "Id", "Div", lecture.BatchId);
+            ViewBag.SubjectId = new SelectList(db.Subjects, "Id", "Name", lecture.SubjectId);
+            ViewBag.TeacherId = new SelectList(db.Teachers, "Id", "Name", lecture.TeacherId);
+            return View(lecture);
+        }
+
+        // POST: Lectures/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,TeacherId,SubjectId,ScheduleDate,StartTime,EndTime,Hours,isDeleted,CreatedBy,CreatedDate,ModifiedBy,ModifiedDate,BatchId")] Lecture lecture)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lecture).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BatchId = new SelectList(db.Batches, "Id", "Div", lecture.BatchId);
+            ViewBag.SubjectId = new SelectList(db.Subjects, "Id", "Name", lecture.SubjectId);
+            ViewBag.TeacherId = new SelectList(db.Teachers, "Id", "Name", lecture.TeacherId);
+            return View(lecture);
+        }
+
+        // GET: Lectures/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lecture lecture = db.Lectures.Find(id);
+            if (lecture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lecture);
+        }
+
+        // POST: Lectures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Lecture lecture = db.Lectures.Find(id);
+            db.Lectures.Remove(lecture);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
